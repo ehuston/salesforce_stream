dde5ff4e5ba4835814d6eb0616a64a13cf04876e
Jeff Douglas <jeff@jeffdouglas.com>
2012-08-26 22:00:29 -0400

    changed readme

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 exports.PORT = process.env.PORT || 3001; // use heroku's dynamic port or 3001 if localhost
 exports.DEBUG = true; 
 exports.ENVIRONMENT = 'production'; 
-exports.CALLBACK_URL = 'http://node-streaming-socketio.herokuapp.com'; 
+exports.CALLBACK_URL = 'http://localhost:3001'; 
 exports.PUSH_TOPIC = '/topic/AllAccounts';
 
-exports.CLIENT_ID = '3MVG9VmVOCGHKYBSMFoH607T0ClprHSETbv.ahZ5By2cYTpciutIxGxLTBGxX23uJnuamiMwSPjQZF2zd4k_l';
-exports.CLIENT_SECRET = '4396035111382732809';
-exports.USERNAME = 'jeff@jeffdouglas.com';
-exports.PASSWORD = 'rTeU886@!FNHX1jAwrxfAd1CYHlFzV1sOh'; 
+exports.CLIENT_ID = YOUR-SFDC-CLIENT-ID;
+exports.CLIENT_SECRET = YOUR-SFDC-CLIENT-SECRET;
+exports.USERNAME = YOUR-SFDC-USERNAME;
+exports.PASSWORD = YOUR-SFDC-PASSWORD-AND-TOKEN; 
