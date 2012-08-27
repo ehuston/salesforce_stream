584a356ae0887bd0d901bd41d215da95f8ced084
Jeff Douglas <jeff@jeffdouglas.com>
2012-08-26 20:25:42 -0400

    added heroku oauth tokens

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 exports.PORT = process.env.PORT || 3001; // use heroku's dynamic port or 3001 if localhost
 exports.DEBUG = true; 
 exports.ENVIRONMENT = 'production'; 
-exports.CALLBACK_URL = 'http://localhost:3001'; 
+exports.CALLBACK_URL = 'http://node-streaming-socketio.herokuapp.com'; 
 exports.PUSH_TOPIC = '/topic/AllAccounts';
 
-exports.CLIENT_ID = '3MVG9VmVOCGHKYBSMFoH607T0CqCsvPhARyhYUtuC_WukyqyFkEEECs.FwOl5uVFCfW86vFxSIXN_g9FJASa_';
-exports.CLIENT_SECRET = '2517772399247411749';
+exports.CLIENT_ID = '3MVG9VmVOCGHKYBSMFoH607T0ClprHSETbv.ahZ5By2cYTpciutIxGxLTBGxX23uJnuamiMwSPjQZF2zd4k_l';
+exports.CLIENT_SECRET = '4396035111382732809';
 exports.USERNAME = 'jeff@jeffdouglas.com';
 exports.PASSWORD = 'rTeU886@!FNHX1jAwrxfAd1CYHlFzV1sOh'; 
