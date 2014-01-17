f5418b3fa81f0b1536d7b52e56ba5868250ecd57
ehuston <ehuston@gmail.com>
2014-01-17 16:21:53 -0600

    setting for heroku

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ exports.PORT = process.env.PORT || 3000; // use heroku's dynamic port or 3000 if
 exports.DEBUG = true; 
 exports.ENVIRONMENT = 'sandbox'; 
 //exports.CALLBACK_URL = 'http://localhost:3000';
-exports.CALLBACK_URL = 'http://shrouded-cliffs-2777.herokuapp.com';
+exports.CALLBACK_URL = 'http://salesforce-streaming.herokuapp.com';
 exports.PUSH_TOPIC1 = '/topic/Accounts';
 exports.PUSH_TOPIC2 = '/topic/Contacts';
 
