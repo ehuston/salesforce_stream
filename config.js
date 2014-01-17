a806fabe18411c93271d31141c30524f5dfab23d
ehuston <ehuston@gmail.com>
2014-01-17 10:26:54 -0600

    text change

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 exports.PORT = process.env.PORT || 3000; // use heroku's dynamic port or 3000 if localhost
 exports.DEBUG = true; 
 exports.ENVIRONMENT = 'sandbox'; 
-exports.CALLBACK_URL = document.URL;//'http://localhost:3000';  - 'http://shrouded-cliffs-2777.herokuapp.com'
+exports.CALLBACK_URL = 'http://shrouded-cliffs-2777.herokuapp.com';//'http://localhost:3000';
 exports.PUSH_TOPIC = '/topic/Accounts';
 
 exports.CLIENT_ID = '3MVG9GiqKapCZBwHQfSepaUSH8aIsJiGkPK4FYQ.tA0HIX7St82g2U0ovwfa_lAwPNVjDbD.0EwJnw0m10WRC';
