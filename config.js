9bebf78c0bc8f728d2d5760946d63074d5dea623
ehuston <ehuston@gmail.com>
2014-01-17 09:45:52 -0600

    test configs

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 exports.PORT = process.env.PORT || 3001; // use heroku's dynamic port or 3001 if localhost
 exports.DEBUG = true; 
-exports.ENVIRONMENT = 'production'; 
+exports.ENVIRONMENT = 'sandbox'; 
 exports.CALLBACK_URL = 'http://localhost:3001'; 
-exports.PUSH_TOPIC = '/topic/AllAccounts';
+exports.PUSH_TOPIC = '/topic/Accounts';
 
-exports.CLIENT_ID = YOUR-SFDC-CLIENT-ID;
-exports.CLIENT_SECRET = YOUR-SFDC-CLIENT-SECRET;
-exports.USERNAME = YOUR-SFDC-USERNAME;
-exports.PASSWORD = YOUR-SFDC-PASSWORD-AND-TOKEN; 
+exports.CLIENT_ID = '3MVG9GiqKapCZBwHQfSepaUSH8aIsJiGkPK4FYQ.tA0HIX7St82g2U0ovwfa_lAwPNVjDbD.0EwJnw0m10WRC';
+exports.CLIENT_SECRET = '7167716120398501039';
+exports.USERNAME = 'evan@sparefoot.com.develop';
+exports.PASSWORD = 't00ferapsx2uaCxpykiquXpsqCzozWXvC'; 
